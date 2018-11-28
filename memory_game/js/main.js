3a594ff5675eea1644659457b6477e22cff16daa
Brendan Quirk <brendanquirk@Brendans-MBP.home>
2018-11-27 21:52:29 -0500

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -19,7 +19,25 @@ var cards = [{
     cardImage: "images/king-of-diamonds.png"
   }
 ];
+
 var cardsInPlay = [];
+
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+
+    cardElement.setAttribute('src', "images/back.png");
+
+    cardElement.setAttribute('img', "images/queen-of-hearts.png");
+    cardElement.setAttribute('img', "images/queen-of-diamonds.png");
+    cardElement.setAttribute('img', "images/king-of-hearts.png");
+    cardElement.setAttribute('img', "images/king-of-diamonds.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click' , flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
@@ -27,13 +45,17 @@ var checkForMatch = function() {
     alert("Sorry, try again.");
   }
 }
+
 var cardOne = cards[0];
-var flipCard = function(cardId) {
+
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
   checkForMatch();
   console.log("User flipped " + cards[cardId].rank);
   console.log(cards[cardId].suit);
   console.log(cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage);
+
 }
-flipCard(0);
-flipCard(2);
+createBoard();
